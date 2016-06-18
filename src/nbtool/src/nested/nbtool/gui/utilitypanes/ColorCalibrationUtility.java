ae3be01fc6383916a24b4fda65cb06c577eae694
Cory Alini <calini@bowdoin.edu>
2016-06-18 11:46:24 -0400

    changing numbas

--- src/nbtool/src/nested/nbtool/gui/utilitypanes/ColorCalibrationUtility.java
+++ src/nbtool/src/nested/nbtool/gui/utilitypanes/ColorCalibrationUtility.java
@@ -562,8 +562,7 @@ public class ColorCalibrationUtility extends UtilityProvider<ColorParam.Set, Col
 				tab.imageSplitPane.setRightComponent(imageDisplayNorm);
 				
 				tab.imageSplitPane.setResizeWeight(0.5);
-				tab.imageSplitPane.setEnabled(false);
-				tab.imageSplitPane.setBackground(Color.BLACK);
+//				tab.imageSplitPane.setBackground(Color.BLACK);
 				tab.imageSplitPane.setForeground(Color.BLACK);
 				
 				tab.imageSplitPane.setDividerLocation(0.5);
